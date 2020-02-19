113f4ae8b41611d5202e160767d95b15029fcdcf
euglenach <root@DESKTOP-SRAF5NG.localdomain>
2020-02-20 06:24:36 +0900

    ラムダ式練習シーン

---
+++ UniRx_Lecture/Assets/Chapter0/Scripts/LambdaSample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LambdaSample : MonoBehaviour{
+    [SerializeField] private Text text;
+    private readonly Subject<Unit> stream = new Subject<Unit>();
+    public IObservable<Unit> Stream => stream;
+    
+    private void Start(){
+        
+        var teaArray = new[]{"十六","爽健美","生", "お～いお","午後の紅","太陽のマテ"};
+    
+
+        // foreach(var tea in teaArray){ text.text += tea; } // テキストに表示(確認用に使ってください)
+    }
+}
